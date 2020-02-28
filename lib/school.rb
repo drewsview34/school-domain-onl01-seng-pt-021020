d242b7dceb4d80f18a1bb5d5d1f0d3511789e2a1
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-28 23:57:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster = {}
+  end
+end
+
+school = School.new("Bayside High School")
