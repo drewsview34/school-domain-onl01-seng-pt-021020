2ba4e51f87a83d1af2042632424d804d7b6f825b
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-29 00:40:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,10 +7,10 @@ class School
   end
   
   def add add_student(student_name, grade)
-    @roster[grade] ||= []
-    @roster[grade] << student_name
+    roster[grade] ||= []
+    roster[grade] << student_name
   end
-  
+
   def grade(students_in_this_grade)
     @roster[students_in_this_grade]
   end
